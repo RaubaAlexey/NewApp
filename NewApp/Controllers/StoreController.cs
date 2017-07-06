8ed9426a33d7a67057ba349c401b98ae8d054b4f
RaubaAlexey <www.rauba@mail.ru>
2017-07-06 16:23:18 +0300

    create app #1

---
+++ NewApp/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewApp.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "Store page";
+        }
+
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Genre = " + genre);//params
+            return message;
+        }
+
+        public string Details(int id)
+        {
+            string message = "Store.Details, ID = " + id;//url
+            return message;
+        }
+    }
+}
